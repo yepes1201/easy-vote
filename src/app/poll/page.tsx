3662ddbd9b11c2b4f44ace0926ee340b64f70d65
Daniel Yepes <yepes1201@gmail.com>
2024-08-01 20:53:06 -0500

    feat: build create/edit view

---
+++ src/app/poll/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function PollPage() {
+  return (
+    <section className="flex w-full gap-4 justify-between">
+      <nav>Nav</nav>
+      <main>Poll Voting</main>
+      <aside>Chart</aside>
+    </section>
+  );
+}
